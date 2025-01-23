f167be9d70ea4daa9b23272542e193626b75fe19
amaanshahsr <amaancreate@gmail.com>
2025-01-23 22:43:23 +0530

    fix:possible fix for vercel deployment (14)

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { Suspense } from "react";
 import { Toaster } from "react-hot-toast";
 import Card from "../components/Card/card";
 import { Footer } from "../components/footer";
@@ -6,14 +8,16 @@ import { Header } from "../components/header";
 
 const Page = () => {
   return (
-    <main className="w-full min-h-screen relative sm:border-x border-gray-200 ">
-      <Toaster />
-      <Header />
-      <section className="flex flex-col items-center w-full  h-[calc(100vh-8rem)] overflow-scroll ">
-        <Card />
-      </section>
-      <Footer />
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="w-full min-h-screen relative sm:border-x border-gray-200 ">
+        <Toaster />
+        <Header />
+        <section className="flex flex-col items-center w-full  h-[calc(100vh-8rem)] overflow-scroll ">
+          <Card />
+        </section>
+        <Footer />
+      </main>
+    </Suspense>
   );
 };
 
